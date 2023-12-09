98f083fb95cdc6c6af79c20b47b2235266909bee
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-09 23:03:43 +0530

    Update objectANDkeyBased.js

--- objectANDkeyBased.js
+++ objectANDkeyBased.js
@@ -15,3 +15,20 @@ for (var i = 0; i <= 10; i++) {
 Object.keys(ob).forEach((key) => {
     console.log(key, ob[key].Even, ob[key].Odd);
 });
+
+
+/*
+[
+  { key: 'b08vka', even: [ 0 ], odd: [] },
+  { key: 'gviwq', even: [], odd: [ 1 ] },
+  { key: '9mco7r', even: [ 2 ], odd: [] },
+  { key: '57zi9g', even: [], odd: [ 3 ] },
+  { key: 'qgz4h', even: [ 4 ], odd: [] },
+  { key: '4zs7qk', even: [], odd: [ 5 ] },
+  { key: 'unjtpi', even: [ 6 ], odd: [] },
+  { key: '52kcni', even: [], odd: [ 7 ] },
+  { key: 'v03dtc', even: [ 8 ], odd: [] },
+  { key: '2c5f8i', even: [], odd: [ 9 ] },
+  { key: 'wbh5hh', even: [ 10 ], odd: [] }
+]
+*/
