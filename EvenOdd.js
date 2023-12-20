3c29689dd88d58c3540528a5db665e9ad0794561
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-20 11:32:30 +0530

    Create EvenOdd.js

---
+++ EvenOdd.js
@@ -0,0 +1,30 @@
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const ar=[2,3,4,5,1,6,7,8,9,0];
+const even=[]; const odd=[];
+for(var i=0;i<ar.length;i++)
+{
+    if(ar[i]%2){	 even.push(ar[i]);	}
+    else{	odd.push(ar[i]);	}
+} 
+even.sort(); 
+odd.sort(); 
+console.log(even); 
+console.log(odd);
+console.log("Maximum Even number: ",even[even.length-1]); 
+console.log("Minimum Even number: ",even[0]);
+console.log("Maximum Odd number: ",odd[odd.length-1]); 
+console.log("Minimun Odd number: ",odd[0]);
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+let ob = [{arr: [2, 3, 4, 5, 1, 6, 7, 8, 9, 0], even: [], odd: [], MaximumEven: undefined, MinimumEven: undefined, MaximumOdd: undefined, MinimumOdd: undefined}];
+for(let i=0;i<ob[0].arr.length-1;i++)
+    {
+        if(ob[0].arr[i]%2==0) ob[0].even.push(ob[0].arr[i]); else ob[0].odd.push(ob[0].arr[i]);
+    }
+ob[0].even.sort();
+ob[0].odd.sort();
+ob[0].MaximumEven=ob[0].even[ob[0].even.length-1];
+ob[0].MinimumEven=ob[0].even[0];
+ob[0].MaximumOdd=ob[0].odd[ob[0].odd.length-1];
+ob[0].MinimumOdd=ob[0].odd[0];
+console.log(ob[0]);
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
