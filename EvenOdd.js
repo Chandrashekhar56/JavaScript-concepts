e5989498d3f54a87a405d35baa3e534ecf6b7172
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-20 11:35:29 +0530

    Update EvenOdd.js

--- EvenOdd.js
+++ EvenOdd.js
@@ -28,3 +28,18 @@ ob[0].MaximumOdd=ob[0].odd[ob[0].odd.length-1];
 ob[0].MinimumOdd=ob[0].odd[0];
 console.log(ob[0]);
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+let ob = {arr: [2, 3, 4, 5, 1, 6, 7, 8, 9, 0], even: [], odd: [], MaximumEven: undefined, MinimumEven: undefined, MaximumOdd: undefined, MinimumOdd: undefined};
+for(let i=0;i<ob.arr.length-1;i++)
+    {
+        if(ob.arr[i]%2==0)
+         ob.even.push(ob.arr[i]); 
+        else 
+         ob.odd.push(ob.arr[i]);
+    }
+ob.even.sort();
+ob.odd.sort();
+ob.MaximumEven=ob.even[ob.even.length-1];
+ob.MinimumEven=ob.even;
+ob.MaximumOdd=ob.odd[ob.odd.length-1];
+ob.MinimumOdd=ob.odd;
+console.log(ob);
