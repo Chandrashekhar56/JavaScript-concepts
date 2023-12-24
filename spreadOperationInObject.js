6be13e616a076b4f4228d6c77d6dba6116d70084
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-25 00:01:52 +0530

    Create spreadOperationInObject.js

---
+++ spreadOperationInObject.js
@@ -0,0 +1,32 @@
+let ob={
+    person1:{
+     name:"shekhar1",
+     age: 24,
+    },
+  person2:{
+        name:"sachin1",
+        age:24,
+    },
+    person3:{
+     name:"shekhar2",
+     age: 24,
+    },
+  person4:{
+        name:"sachin2",
+        age:24,
+    },
+};
+console.log(ob);
+
+// let ob1={...{person1:{...ob.person1}}};
+let ob1={person1:{...ob.person1}};
+console.log(ob1);
+
+let ob2={person1:{...ob.person1},...ob};
+console.log(ob2);
+
+let ob3={...ob, person1:{...ob.person1,name:"krishna"}};
+console.log(ob3);
+
+let ob4={...ob};
+console.log(ob4);
