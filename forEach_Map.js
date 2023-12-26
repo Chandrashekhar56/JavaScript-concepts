e7c56ce00e249856d158be6ca42dcad685ca3a78
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-26 11:54:49 +0530

    Update forEach_Map.js

--- forEach_Map.js
+++ forEach_Map.js
@@ -13,3 +13,41 @@ arr.map((e,index)=>{
     console.log(index+" "+e.firstname+" "+e.lastname+" "+e.age);
 });
 
+\\=======================================================================================================\\
+        
+let ar=['value1','value2','value3','value4'];
+let ob1=[...ar];
+let ob2={...ar, 4:'value5'};
+
+console.log(ob1);
+ob1.forEach((item,index)=> console.log(item));
+
+console.log("\\----------------------\\-------------------------\\");
+
+console.log(ob2);
+ob1.map((item,index)=> console.log(index +" :  "+ item));
+
+/*        output:
+
+[ 'value1', 'value2', 'value3', 'value4' ]
+value1
+value2
+value3
+value4
+
+\----------------------\-------------------------\
+
+{
+  '0': 'value1',
+  '1': 'value2',
+  '2': 'value3',
+  '3': 'value4',
+  '4': 'value5'
+}
+0 :  value1
+1 :  value2
+2 :  value3
+3 :  value4
+
+
+*/
