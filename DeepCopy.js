90f2fcc1d0763de8008ccd73e20ad066e8710f1b
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-26 22:39:59 +0530

    Update DeepCopy.js

--- DeepCopy.js
+++ DeepCopy.js
@@ -1,5 +1,5 @@
 
-//--------------------------Deep Copy-------------------------------------------//
+//=======================================================Deep Copy========================================================//
 const vehicleOne1 = {
   brand: 'Ford',
   model: 'Mustang',
@@ -12,5 +12,57 @@ const vehicleOne2 = JSON.parse(JSON.stringify(vehicleOne1));
 
 vehicleOne2.brand = 'BMW';
 
-console.log(vehicleOne2);
-console.log(vehicleOne1);
+console.log("vehicleOne2: ",vehicleOne2);
+console.log("vehicleOne1: ",vehicleOne1);
+
+/*
+    output:
+vehicleOne2:  {
+  brand: 'BMW',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red'
+}
+vehicleOne1:  {
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red'
+}
+*/
+
+//=======================================================Deep Copy========================================================//
+
+function deepClone(obj) 
+{
+  return JSON.parse(JSON.stringify(obj));
+}
+
+const originalObject ={
+  name: "John",
+  age: 25,
+  address: {
+    city: "New York",
+    country: "USA"
+  }
+};
+const clonedObject = deepClone(originalObject);
+clonedObject.name='shekhar';
+console.log("cloned Object: ",clonedObject );
+console.log("original Object: ",originalObject);
+/*
+    output:
+cloned Object:  {
+  name: 'shekhar',
+  age: 25,
+  address: { city: 'New York', country: 'USA' }
+}
+original Object:  {
+  name: 'John',
+  age: 25,
+  address: { city: 'New York', country: 'USA' }
+}
+*/
+//=======================================================Deep Copy========================================================//
