876e50d32136987103f868e86cc1dbe13b6c1e64
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-26 10:33:15 +0530

    Create ArrayDestructuring.js

---
+++ ArrayDestructuring.js
@@ -0,0 +1,13 @@
+const user = ['john_doe','John','Doe',21];
+const [username,firstname,lastname,age]=user;
+console.log(username);
+console.log(lastname);
+console.log(firstname);
+console.log(age);
+
+/*
+john_doe
+Doe
+John
+21
+*/
