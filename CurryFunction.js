100e32e2b9f46409ceecd91462d0aae3710a92d9
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-26 12:28:39 +0530

    Update CurryFunction.js

--- CurryFunction.js
+++ CurryFunction.js
@@ -1,3 +1,5 @@
+\\=================================================================================================================================================\\
+
 function curryAdd(x) {
   return function(y) {
     return function(z) {
@@ -6,4 +8,42 @@ function curryAdd(x) {
   };
 }
 const curriedAdd = curryAdd(5)(3)(2);
-console.log(curriedAdd);  /*    output:    10    */
+console.log(curriedAdd);  /*  output:  10  */
+
+\\=================================================================================================================================================\\
+ 
+function curryAdd(x) {
+  return function(y) {
+    return function(z) {
+      return x + y;
+    };
+  };
+}
+const curriedAdd = curryAdd(5)(3)(2);
+console.log(curriedAdd); /*  output:  8  */  
+
+\\=================================================================================================================================================\\
+
+  function curryAdd(x) {
+  return function(y) {
+    return function(z) {
+      return x;
+    };
+  };
+}
+const curriedAdd = curryAdd(5)(3)(2);
+console.log(curriedAdd); /*  output:  5  */  
+
+\\=================================================================================================================================================\\
+
+function curryAdd(x) {
+  return function(y) {
+    return function(z) {
+      return 'by by';
+    };
+  };
+}
+const curriedAdd = curryAdd(5)(3)(2);
+console.log(curriedAdd); /*  output:  by by  */
+
+\\=================================================================================================================================================\\
