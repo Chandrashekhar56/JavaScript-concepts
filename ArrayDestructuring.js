83fb486c0479522d457e71b9b3a3e1e6c0ec9348
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2023-12-28 14:54:31 +0530

    Update ArrayDestructuring.js

--- ArrayDestructuring.js
+++ ArrayDestructuring.js
@@ -11,3 +11,39 @@ Doe
 John
 21
 */
+
+\\=============================================================================================================================\\
+
+const arr= [
+  "Oluwatobi",
+  "Sofela",
+  "codesweetly.com",
+];
+const [firstName1, lastName1, website1] =arr;
+console.log(firstName1);
+console.log(lastName1);
+console.log(website1);
+
+const [firstName2, lastName2, website2] = [
+  "Oluwatobi",
+  "Sofela",
+  "codesweetly.com",
+];
+console.log(firstName2);
+console.log(lastName2);
+console.log(website2);
+
+/*
+  Output:
+
+Oluwatobi
+Sofela
+codesweetly.com
+
+
+Oluwatobi
+Sofela
+codesweetly.com
+*/
+
+\\=============================================================================================================================\\
