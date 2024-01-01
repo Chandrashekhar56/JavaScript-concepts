f3e0b952d541aebffc4cdc72a92b168a021b1af1
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2024-01-01 22:07:31 +0530

    Create filter_map_reduce.js

---
+++ filter_map_reduce.js
@@ -0,0 +1,24 @@
+
+//======================================================================================================================//
+
+const numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = numbers.filter((num )=> num % 2 === 0);
+console.log(evenNumbers);
+
+const sum1=numbers.reduce((acc,curr)=>{return curr+1},0);
+console.log("sum: ",sum1);
+
+const sum2=numbers.map((item,index)=>{return item+1});
+console.log(sum2);
+
+/*
+output:
+
+[ 0, 2, 4, 6, 8, 10 ]
+sum:  11
+[ 1, 2, 3, 4,  5, 6, 7, 8, 9, 10, 11 ]
+
+*/
+
+//======================================================================================================================//
