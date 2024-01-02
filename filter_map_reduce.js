02b6e07448aa61a482f44275e2151f483e74b0b0
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2024-01-02 16:00:43 +0530

    Update filter_map_reduce.js

--- filter_map_reduce.js
+++ filter_map_reduce.js
@@ -12,12 +12,20 @@ console.log("sum: ",sum1);
 const sum2=numbers.map((item,index)=>{return item+1});
 console.log(sum2);
 
+const sum2=[]
+numbers.forEach((item,index)=>{sum2.push(item+1)});
+console.log(sum2);
+
 /*
 output:
 
 [ 0, 2, 4, 6, 8, 10 ]
+
 sum:  11
+
 [ 1, 2, 3, 4,  5, 6, 7, 8, 9, 10, 11 ]
+
+[  1, 2, 3, 4,  5,  6, 7, 8, 9, 10,  11  ]
 
 */
 
