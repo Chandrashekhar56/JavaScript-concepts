33dad333518fd699aca922317c9babb5fe4da491
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2024-01-14 22:42:58 +0530

    Update ReferenceValue.js

--- ReferenceValue.js
+++ ReferenceValue.js
@@ -18,3 +18,26 @@ output:
 
 */
 //====================================================================================================================================//
+
+const user=[{username:'shekhar', age:20}];
+const user1=user;
+
+user1.username='sachin';
+user1.age=20;
+
+console.log(user1);
+console.log("Username:",user1.username," ","Age:",user1.age);
+
+console.log(user);
+console.log("Username:",user.username," ","Age:",user.age);
+
+/*
+output:
+
+[ { username: 'shekhar', age: 20 }, username: 'sachin', age: 20 ]
+Username: sachin   Age: 20
+[ { username: 'shekhar', age: 20 }, username: 'sachin', age: 20 ]
+Username: sachin   Age: 20
+*/
+
+//====================================================================================================================================//
