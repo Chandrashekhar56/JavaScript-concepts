ee16bd81e23795439b1b9ef900cf74b9524cfd82
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2024-02-15 16:26:36 +0530

    Update shallowcopy.js

--- shallowcopy.js
+++ shallowcopy.js
@@ -15,4 +15,56 @@ vehicleOne2.brand='BMW';
 
 console.log(vehicleOne2);
 console.log(vehicleOne1);
+
+/*
+output:
+{
+  brand: 'BMW',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red'
+}
+{
+  brand: 'BMW',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red'
+}
+*/
 //--------------------------------------------------------------//
+
+let oA = { id: 1, name: "shekhar", age: 21 };
+let dA = oA; 
+console.log(oA === dA); 
+dA = { ...dA, name: "bawa" }; 
+console.log(oA === dA); 
+console.log(oA); 
+console.log(dA); 
+/*
+output:
+
+true
+false
+{ id: 1, name: 'shekhar', age: 21 }
+{ id: 1, name: 'bawa', age: 21 }
+
+*/
+//--------------------------------------------------------------//
+
+let oA={id:1,name:"shekhar",age:21};
+let dA=oA;
+console.log(dA===oA);
+dA.name="bawa";
+console.log(dA===oA);
+
+/*
+output:
+
+true
+true
+
+*/
+//--------------------------------------------------------------//
+
