86698111b149b29f887bfff066e107ff34005008
Chandrashekhar sati <78782870+Chandrashekhar56@users.noreply.github.com>
2024-02-21 18:00:50 +0530

    reduce example interview Question
    reduce example interview Question

--- interviewQuestion4.js
+++ interviewQuestion4.js
@@ -1,4 +1,34 @@
+//=====================================================================================================================================//
 
+const numbers = [1, 2, 3, 4, 5];
+const max = numbers.reduce((accumulator, currentValue) => {
+    // console.log(accumulator, currentValue);
+  return currentValue > accumulator ? currentValue : accumulator;
+}, numbers[0]); 
+console.log(max);
+/*
+output:
+5
+
+*/
+//=====================================================================================================================================//
+
+const numbers = [1, 2, 3, 4, 5];
+const max = numbers.reduce((accumulator, currentValue) => {
+    if (currentValue > accumulator) {
+        return currentValue;
+    } else {
+        return accumulator;
+    }
+}, numbers[0]);
+
+console.log(max);
+
+/*
+output:
+5
+
+*/
 //=====================================================================================================================================//
 
 const numbers = [1, 2, 3, 4, 5];
